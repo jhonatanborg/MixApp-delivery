74b97d034c8c658a1f9e2ac7694a26f697ce8958
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-05-27 14:05:55 -0300

    constants styles added in app  💎

---
+++ src/styles/colors.js
@@ -0,0 +1,20 @@
+export const PRIMARY = "#1779ba";
+export const SECONDARY = "#767676";
+export const WHITE = "#FFFFFF";
+export const BLACK = "#111a2c";
+export const DARK_BLUE = "#363F5F";
+
+// THEME
+export const THEMECOLOR = "#FF6C44";
+export const THEME_LIGHT = "#ffede8";
+export const BACKGROUND_APP = "#FBFBFB";
+
+// ACTIONS
+export const SUCCESS = "#3adb76";
+export const WARNING = "#ffae00";
+export const ALERT = "#cc4b37";
+
+// GRAYSCALE
+export const GRAY_LIGHT = "#F5F5F8";
+export const GRAY_MEDIUM = "#cacaca";
+export const GRAY_DARK = "#8a8a8a";
---
+++ src/styles/typography.js
@@ -0,0 +1,34 @@
+import { scaleFont } from "./mixins";
+
+// FONT FAMILY
+export const FONT_FAMILY_REGULAR = "Jost_400Regular";
+export const FONT_FAMILY_BOLD = "Jost_600SemiBold";
+
+// FONT WEIGHT
+export const FONT_WEIGHT_REGULAR = "400";
+export const FONT_WEIGHT_BOLD = "700";
+
+// FONT SIZE
+export const FONT_SIZE_12 = scaleFont(12);
+export const FONT_SIZE_14 = scaleFont(14);
+export const FONT_SIZE_16 = scaleFont(16);
+export const FONT_SIZE_22 = scaleFont(22);
+export const FONT_SIZE_32 = scaleFont(32);
+export const FONT_SIZE_40 = scaleFont(40);
+export const FONT_SIZE_50 = scaleFont(50);
+
+// LINE HEIGHT
+export const LINE_HEIGHT_24 = scaleFont(24);
+export const LINE_HEIGHT_20 = scaleFont(20);
+export const LINE_HEIGHT_16 = scaleFont(16);
+
+// FONT STYLE
+export const FONT_REGULAR = {
+  fontFamily: FONT_FAMILY_REGULAR,
+  fontWeight: FONT_WEIGHT_REGULAR,
+};
+
+export const FONT_BOLD = {
+  fontFamily: FONT_FAMILY_BOLD,
+  fontWeight: FONT_WEIGHT_BOLD,
+};
