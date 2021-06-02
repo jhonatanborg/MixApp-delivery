6459525799c3428de8809fb9eb5e4645797b5d5f
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-06-02 10:04:12 -0300

    new actions app store

---
+++ src/store/actions/addressActions.js
@@ -0,0 +1,8 @@
+export const setCoords = (coords) => ({
+  type: "SET_COORDS",
+  payload: coords,
+});
+export const setAddress = (address) => ({
+  type: "SET_ADDRESS",
+  payload: address,
+});
--- src/store/actions/saleActions.js
+++ src/store/actions/saleActions.js
@@ -2,11 +2,19 @@ export const addItem = (item) => ({
   type: "ADD_ITEM_SALE",
   payload: item,
 });
-export const removeItem = (item) => ({
+export const removeItem = (index) => ({
   type: "REMOVE_ITEM_SALE",
+  payload: index,
+});
+export const resetSale = (item) => ({
+  type: "RESETE",
   payload: item,
 });
-export const reseteSale = () => ({
-  type: "RESETE",
-  payload: [],
+export const AddQtdItem = (item) => ({
+  type: "ADD_QUANTITY",
+  payload: item,
+});
+export const SubQtdItem = (item) => ({
+  type: "SUB_QUANTITY",
+  payload: item,
 });
