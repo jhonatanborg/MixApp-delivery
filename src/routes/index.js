82b4946f58299981aed60e881dfd4cec602e3740
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-06-02 10:03:17 -0300

    new routes app

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { View, ActivityIndicator } from "react-native";
 
-import { AppRoutes } from "./app-routes";
+import AppRoutes from "./app-routes";
 import AuthRoutes from "./auth-routes";
 
 import AuthContext from "../contexts/";
