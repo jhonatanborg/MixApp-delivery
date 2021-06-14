c1a3f11dd6a86c3dd0c66ea172c23ae1b3a2c192
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-06-14 20:48:19 -0300

    render items response

--- src/screens/sale/payment/Payment.jsx
+++ src/screens/sale/payment/Payment.jsx
@@ -2,24 +2,25 @@ import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
-  Image,
   TouchableOpacity,
-  TextInput,
   ScrollView,
-  SafeAreaView,
   FlatList,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 const { BASE_URL } = getEnvVars();
 import getEnvVars from "../../../../environment";
 import { SvgUri } from "react-native-svg";
 
 import styles from "./Payment.style";
 import Button from "../../../components/atoms/Button/Button";
+import * as saleActions from "../../../store/actions/saleActions";
 
 const Payment = (props) => {
+  console.log(props);
   const company = useSelector((state) => state.company);
+  const dispatch = useDispatch();
+  console.log(dispatch);
   const [payments, setPayments] = useState([]);
   const [paySelected, setPaySelected] = useState([]);
   useEffect(() => {
@@ -33,7 +34,6 @@ const Payment = (props) => {
         group.payments = [];
       });
     });
-
     groups.map((group) => {
       newPayments.map((payment) => {
         if (group.id === payment.payment.payment_group_available_id) {
@@ -41,6 +41,7 @@ const Payment = (props) => {
             id: payment.payment.id,
             title: payment.payment.title,
             img: payment.payment.img,
+            select: false,
           });
         }
       });
@@ -48,29 +49,54 @@ const Payment = (props) => {
 
     setPayments(groups);
   }, []);
-  function selectPay(value) {
-    const pay = paySelected;
-    if (
-      pay.length > 0 &&
-      pay.filter((item) => item.id === value.id).length === 0
-    ) {
-      pay.push(value);
-    } else if (pay.length === 0) {
-      pay.push(value);
-    } else if (
-      pay.length > 0 &&
-      pay.filter((item) => item.id === value.id).length > 0
-    ) {
-      pay.forEach((item, index) => {
-        if (item.id === value.id) {
-          pay.splice(index, 1);
+  function selectPay(value, indexGroup, indexItem) {
+    const newPayments = [...payments];
+    let lastItemSelected;
+    let lastGroupSelected;
+    let totalSelected = 0;
+    let alreadyExists = false;
+    payments.forEach((group, indexGroup) => {
+      group.payments.forEach((pay, indexPay) => {
+        if (pay.select) {
+          lastGroupSelected = indexGroup;
+          lastItemSelected = indexPay;
+          totalSelected += 1;
+          if (pay.id === value.id) alreadyExists = true;
         }
       });
+    });
+    if (totalSelected < 2) {
+      if (alreadyExists) {
+        newPayments[indexGroup].payments[indexItem].select = false;
+        totalSelected -= 1;
+      } else {
+        newPayments[indexGroup].payments[indexItem].select = true;
+        totalSelected += 1;
+      }
+    } else if (totalSelected === 2) {
+      totalSelected -= 1;
+      if (alreadyExists) {
+        newPayments[indexGroup].payments[indexItem].select = false;
+      } else {
+        newPayments[indexGroup].payments[indexItem].select = true;
+        newPayments[lastGroupSelected].payments[
+          lastItemSelected
+        ].select = false;
+      }
     }
-    if (pay.length > 2) {
-      pay.splice(1, 1);
-    }
-    setPaySelected(pay);
+    setPayments(newPayments);
+  }
+  function handleSendPurchase() {
+    const payment_available_id = [];
+    payments.forEach((group) => {
+      group.payments.forEach((pay) => {
+        if (pay.select) {
+          payment_available_id.push(pay);
+        }
+      });
+    });
+    dispatch(saleActions.AddPay(payment_available_id));
+    props.navigation.goBack();
   }
   return (
     <View style={styles.container}>
@@ -85,36 +111,51 @@ const Payment = (props) => {
         <Text style={styles.heading2}>Selecione a forma de pagamento</Text>
       </View>
       <View style={styles.section}>
-        {payments.map((item) => {
+        {payments.map((group, indexGroup) => {
           return (
-            <View>
+            <View key={indexGroup}>
               <View>
-                <Text style={styles.heading1}>{item.title}</Text>
+                <Text style={styles.heading1}>{group.title}</Text>
               </View>
-              <View>
-                {item.payments.map((pay) => {
-                  return (
-                    <TouchableOpacity
-                      key={pay.index}
-                      onPress={() => selectPay(pay)}
-                      style={styles.card}
-                    >
-                      <View style={styles.image}>
-                        <SvgUri uri={BASE_URL + pay.img} />
-                      </View>
-                      <View>
-                        <Text style={styles.text}>{pay.title}</Text>
-                      </View>
-                    </TouchableOpacity>
-                  );
-                })}
+              <View style={styles.listPayment}>
+                <FlatList
+                  data={group.payments}
+                  numColumns={2}
+                  keyExtractor={(item) => item.id}
+                  renderItem={({ item, index }) => {
+                    return (
+                      <TouchableOpacity
+                        key={item.id}
+                        onPress={() => selectPay(item, indexGroup, index)}
+                        style={item.select ? styles.cardSelected : styles.card}
+                      >
+                        <View style={styles.image}>
+                          <SvgUri uri={BASE_URL + item.img} />
+                        </View>
+                        <View>
+                          <Text
+                            style={
+                              item.select ? styles.textSelected : styles.text
+                            }
+                          >
+                            {item.title}
+                          </Text>
+                        </View>
+                      </TouchableOpacity>
+                    );
+                  }}
+                />
               </View>
             </View>
           );
         })}
       </View>
       <View style={styles.footer}>
-        <Button name="Confirmar" buttonColor={styles.icon} />
+        <Button
+          name="Confirmar"
+          buttonColor={styles.icon}
+          onPress={() => handleSendPurchase()}
+        />
       </View>
     </View>
   );
--- src/screens/sale/payment/Payment.style.js
+++ src/screens/sale/payment/Payment.style.js
@@ -37,10 +37,16 @@ const styles = StyleSheet.create({
     textTransform: "capitalize",
     marginLeft: 8,
   },
-
-  image: {
-    borderRadius: 8,
+  textSelected: {
+    color: Colors.WHITE,
+    fontSize: Typography.FONT_SIZE_22,
+    fontWeight: Typography.FONT_WEIGHT_REGULAR,
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    flex: 3,
+    textTransform: "capitalize",
+    marginLeft: 8,
   },
+
   btn: {
     flex: 1,
     height: 40,
@@ -59,7 +65,7 @@ const styles = StyleSheet.create({
     fontFamily: Typography.FONT_FAMILY_BOLD,
   },
   listPayment: {
-    flexGrow: 1,
+    flexDirection: "row",
   },
   card: {
     marginHorizontal: 5,
@@ -83,47 +89,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
   },
-  cardHeader: {
-    borderBottomColor: Colors.GRAY_DARK,
-    borderBottomWidth: 0.5,
-  },
-  cardTitle: {
-    paddingVertical: 5,
-    color: Colors.GRAY_DARK,
-    fontSize: Typography.FONT_SIZE_22,
-    fontFamily: Typography.FONT_FAMILY_BOLD,
-  },
-  content: {
-    borderRadius: 8,
-    marginVertical: 5,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  icon: {
-    backgroundColor: Colors.GRAY_DARK,
-    padding: 5,
-  },
-  formGroup: {
-    flex: 3,
-    paddingVertical: 20,
-  },
-  input: {
-    height: 40,
-    borderWidth: 0,
-    backgroundColor: Colors.GRAY_LIGHT,
-    borderRadius: 8,
-    paddingHorizontal: 15,
-    color: Colors.GRAY_DARK,
-    fontSize: Typography.FONT_SIZE_22,
-    fontWeight: Typography.FONT_WEIGHT_REGULAR,
-    fontFamily: Typography.FONT_FAMILY_BOLD,
-  },
-  divider: {
-    marginTop: 5,
-    borderColor: Colors.GRAY_DARK,
-    borderTopWidth: 0.5,
-  },
+
   footer: {
     position: "absolute",
     height: 100,
