7396b2ed6ad3a4539c90906b8eda8abbf288c1cb
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-06-14 20:45:23 -0300

    create purchase details screen

---
+++ src/screens/account/purchase/details/PurchaseDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const PurchaseDetails = (props) => {
+  return (
+    <View>
+      <Text>Aqui</Text>
+    </View>
+  );
+};
+export default PurchaseDetails;
