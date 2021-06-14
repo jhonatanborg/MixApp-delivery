a353641c5dcf3f9248e08bb1569bf1e478288889
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-06-14 20:45:05 -0300

    ui profile exact

--- src/screens/account/profile/Profile.jsx
+++ src/screens/account/profile/Profile.jsx
@@ -1,17 +1,60 @@
 import React, { useContext } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./Profile.style";
 import AuthContext from "../../../contexts/index";
+import { Feather } from "@expo/vector-icons";
 
 import Button from "../../../components/atoms/Button/Button";
 const Profile = (props) => {
+  console.log(props);
   const { signed, signOut, user } = useContext(AuthContext);
   async function handleSignOut() {
     await signOut();
   }
   return (
     <View style={styles.container}>
-      <Text>Perfil : {user.name}</Text>
+      <View style={styles.header}>
+        <View style={styles.headerTitle}>
+          <Text style={styles.titleScreen}>Minha conta</Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.cardTitle}>Perfil:</Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.text}> {user.name}</Text>
+          <TouchableOpacity>
+            <Feather name="edit" size={24} color={styles.btnIconColor.color} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity>
+        <View style={styles.card}>
+          <View style={styles.content}>
+            <Text style={styles.text}>Meus endereços</Text>
+            <Feather
+              name="chevron-right"
+              size={24}
+              color={styles.btnIconColor.color}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate("PurchaseList")}
+      >
+        <View style={styles.card}>
+          <View style={styles.content}>
+            <Text style={styles.text}>Meus Pedidos</Text>
+            <Feather
+              name="chevron-right"
+              size={24}
+              color={styles.btnIconColor.color}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
       <Button name="Sair" buttonColor={styles.button} onPress={handleSignOut} />
     </View>
   );
--- src/screens/account/profile/Profile.style.js
+++ src/screens/account/profile/Profile.style.js
@@ -4,11 +4,57 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.GRAY_LIGHT,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
+  },
+  header: {
+    paddingVertical: 15,
+  },
+  titleScreen: {
+    color: Colors.GRAY_DARK,
+    fontSize: Typography.FONT_SIZE_22,
+    fontWeight: Typography.FONT_WEIGHT_REGULAR,
+    fontFamily: Typography.FONT_FAMILY_BOLD,
+  },
+  text: {
+    color: Colors.GRAY_DARK,
+    fontSize: Typography.FONT_SIZE_22,
+    fontWeight: Typography.FONT_WEIGHT_REGULAR,
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    textTransform: "capitalize",
   },
   button: {
     backgroundColor: Colors.THEMECOLOR,
+  },
+  card: {
+    backgroundColor: Colors.WHITE,
+    padding: 15,
+    borderRadius: 8,
+    marginVertical: 5,
+  },
+
+  cardHeader: {
+    borderBottomColor: Colors.GRAY_DARK,
+    borderBottomWidth: 0.5,
+  },
+  cardTitle: {
+    paddingVertical: 5,
+    color: Colors.GRAY_DARK,
+    fontSize: Typography.FONT_SIZE_22,
+    fontFamily: Typography.FONT_FAMILY_BOLD,
+  },
+  content: {
+    borderRadius: 8,
+    marginVertical: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  icon: {
+    backgroundColor: Colors.GRAY_DARK,
+    padding: 5,
+  },
+  btnIconColor: {
+    color: Colors.GRAY_DARK,
   },
 });
 
