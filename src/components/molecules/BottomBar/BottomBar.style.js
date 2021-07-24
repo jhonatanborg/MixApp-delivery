8b38a022d4bec8a77bcfcf7d9f3d3e4b19884dba
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-07-24 19:45:55 -0400

    added props and styles

--- src/components/molecules/BottomBar/BottomBar.style.js
+++ src/components/molecules/BottomBar/BottomBar.style.js
@@ -1,19 +1,20 @@
-import { StyleSheet } from "react-native";
-import { Typography, Colors } from "../../../styles";
+import { StyleSheet } from 'react-native';
+import { Typography, Colors } from '../../../styles';
 
 const styles = StyleSheet.create({
   bottomBar: {
-    position: "absolute",
+    position: 'absolute',
     height: 100,
     bottom: 0,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignContent: "center",
-    alignItems: "center",
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    alignItems: 'center',
     borderRadius: 8,
     backgroundColor: Colors.WHITE,
     paddingHorizontal: 8,
+    zIndex: 10,
   },
   button: {
     paddingHorizontal: 40,
@@ -25,17 +26,17 @@ const styles = StyleSheet.create({
   buttonText: {
     fontFamily: Typography.FONT_FAMILY_BOLD,
     fontSize: 16,
-    textAlign: "center",
+    textAlign: 'center',
     color: Colors.WHITE,
   },
   bottomBarActions: {
     paddingVertical: 2,
     flex: 2,
     marginRight: 8,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    shadowColor: "#000",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: { width: 2, height: 4 },
     shadowOpacity: 0.5,
     shadowRadius: 2,
@@ -46,7 +47,7 @@ const styles = StyleSheet.create({
   quantity: {
     fontFamily: Typography.FONT_FAMILY_BOLD,
     fontSize: 16,
-    textAlign: "center",
+    textAlign: 'center',
     color: Colors.PRIMARY,
   },
   buttomAction: {
