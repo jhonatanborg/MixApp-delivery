d6cb2af4bee8e8d4128756ba835f089a4e7d4c87
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-07-24 19:49:33 -0400

    colors define

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,23 +1,23 @@
-export const PRIMARY = "#1779ba";
-export const SECONDARY = "#767676";
-export const WHITE = "#FFFFFF";
-export const BLACK = "#111a2c";
-export const DARK_BLUE = "#363F5F";
+export const PRIMARY = '#1779ba';
+export const SECONDARY = '#767676';
+export const WHITE = '#FFFFFF';
+export const BLACK = '#111a2c';
+export const DARK_BLUE = '#363F5F';
 
 // THEME
-export const THEMECOLOR = "#5530E5";
-export const THEME_LIGHT = "#FFDED6";
-export const BACKGROUND_APP = "#FBFAFF";
+export const THEMECOLOR = '#FF6C44';
+export const THEME_LIGHT = '#FFDED6';
+export const BACKGROUND_APP = '#FBFAFF';
 
 // ACTIONS
-export const SUCCESS = "#3adb76";
-export const WARNING = "#ffae00";
-export const ALERT = "#cc4b37";
+export const SUCCESS = '#3adb76';
+export const WARNING = '#ffae00';
+export const ALERT = '#cc4b37';
 
 // GRAYSCALE
-export const GRAY_LIGHT = "#F5F5F8";
-export const GRAY_MEDIUM = "#cacaca";
-export const GRAY_DARK = "#8a8a8a";
+export const GRAY_LIGHT = '#F5F5F8';
+export const GRAY_MEDIUM = '#cacaca';
+export const GRAY_DARK = '#8a8a8a';
 
 // TEXT
-export const TEXT_LABEL = "#727272";
+export const TEXT_LABEL = '#727272';
